9034426ee357640f2524cf36a0f62dafcc3db66b
yacinbenhamida <yacinbenhamida@hotmail.fr>
2018-06-27 10:58:26 +0100

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,15 +28,19 @@ export class AppComponent {
   
   calcul(){
     let ri1,ri2,R,B1,B2,mguniv = 0;
+    this.s1 = 0
+    this.s2 = 0;
     mguniv = (this.s1s2+this.s2s3)/2;
     if(mguniv>=15) this.M = 100
     else if(mguniv >10 && mguniv <15 ) this.M = 20*(mguniv-10);
+    else mguniv =0;
     ri1 = (this.r1-1)/this.effectif1;
     ri2 = (this.r2-1)/this.effectif2;
     R = (0.5)*((100-(700*ri1)/3)+(100-(700*ri2)/3))
     if(this.annenaiss == 1997){
       B1 = 5
     }
+    else B1 = 0;
     if(this.moybac>=16){
       B2 = 20;
     }
@@ -49,7 +53,8 @@ export class AppComponent {
     else if (this.moybac <12 && this.moybac >=11){
       B2 = 5
     }
-    this.s1 = 0.3*this.M + 0.7*R + B1 + B2;
+    else B2 = 0;
+    this.s1 = Number((0.3*Number(this.M)) + (0.7*Number(R)) + Number(B1) + Number(B2));
     this.s2 = Number(this.matprincip) + Number(this.matgenerale);
     this.totale = this.s1 + this.s2;
     if(isNaN(this.totale) || this.totale > 165 ||  this.s1>125 || this.s2 >40 || this.totale <= 0 || this.s1 <=0 || this.s2 <=0){
