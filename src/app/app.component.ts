ee55a89443d96dbdcc3baa7de03fc9946de41d64
yacinbenhamida <yacinbenhamida@hotmail.fr>
2018-06-27 10:03:21 +0100

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import { FormGroup, FormBuilder } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  options: FormGroup;
-  title = 'app';
+  s1s2:number;
+  s2s3:number;
   moybac:number;
   M:number = 0;
   r1:number;
@@ -27,12 +27,13 @@ export class AppComponent {
   }
   
   calcul(){
-    let ri1,ri2,R,B1,B2 = 0;
-    if(this.moybac>=15){this.M = 100}
-    else if(this.moybac >10 && this.moybac <15 ) this.M = 20*(this.moybac-10);
+    let ri1,ri2,R,B1,B2,mguniv = 0;
+    mguniv = (this.s1s2+this.s2s3)/2;
+    if(mguniv>=15) this.M = 100
+    else if(mguniv >10 && mguniv <15 ) this.M = 20*(mguniv-10);
     ri1 = (this.r1-1)/this.effectif1;
     ri2 = (this.r2-1)/this.effectif2;
-    R = (0.5)* ((100-(700*ri1)/3)+(100-(700*ri2)/3))
+    R = (0.5)*((100-(700*ri1)/3)+(100-(700*ri2)/3))
     if(this.annenaiss == 1997){
       B1 = 5
     }
@@ -51,11 +52,10 @@ export class AppComponent {
     this.s1 = 0.3*this.M + 0.7*R + B1 + B2;
     this.s2 = Number(this.matprincip) + Number(this.matgenerale);
     this.totale = this.s1 + this.s2;
-
-    if(!isNaN(this.totale)){
-      this.res = true;
+    if(isNaN(this.totale) || this.totale > 165 ||  this.s1>125 || this.s2 >40 || this.totale <= 0 || this.s1 <=0 || this.s2 <=0){
+      alert("veuillez verifier les champs!")
     }
-    else {alert("veuillez verifier les champs!");this.init()}
+    else {this.res = true; }
   }
   init(){
     this.s1 = 0;
@@ -70,5 +70,7 @@ export class AppComponent {
     this.matprincip = null;
     this.matgenerale = null;
     this.moybac = null;
+    this.s1s2 = null;
+    this.s2s3 = null;
   }
 }
